ecf7db17e21d45fa4bd9a5e5f8b24a51c71dd2e0
David Luevano Alvarado <david@luevano.xyz>
2023-12-27 00:12:55 -0600

    feat: add anilist result to result

--- inline/inline.go
+++ inline/inline.go
@@ -8,6 +8,18 @@ import (
 	"github.com/luevano/libmangal"
 )
 
+type QueryResult struct {
+	Query    string        `json:"query"`
+	Provider string        `json:"provider"`
+	Results  []MangaResult `json:"results"`
+}
+
+type MangaResult struct {
+	Index   int                    `json:"index"`
+	Manga   libmangal.Manga        `json:"manga"`
+	Anilist libmangal.AnilistManga `json:"anilist"`
+}
+
 type InlineArgs struct {
 	Query    string
 	Provider string
@@ -22,6 +34,8 @@ type Options struct {
 }
 
 func Run(ctx context.Context, options Options) error {
+	queryResult := QueryResult{Query: options.Query, Provider: options.Provider}
+
 	switch {
 	case options.Download:
 		return fmt.Errorf("unimplemented")
@@ -30,13 +44,30 @@ func Run(ctx context.Context, options Options) error {
 		if err != nil {
 			return err
 		}
-
 		if len(mangas) == 0 {
 			return fmt.Errorf("no mangas found with provider ID %q and query %q", options.Provider, options.Query)
 		}
 
-		mangasJSON, err := json.Marshal(mangas)
-		fmt.Println(string(mangasJSON))
+		mangaResults := []MangaResult{}
+		for i, manga := range mangas {
+			anilists, err := options.Anilist.SearchMangas(ctx, manga.Info().AnilistSearch)
+			if err != nil {
+				return err
+			}
+			mangaResult := MangaResult{Index: i, Manga: manga}
+			if len(anilists) != 0 {
+				mangaResult.Anilist = anilists[0]
+			}
+
+			mangaResults = append(mangaResults, mangaResult)
+		}
+		queryResult.Results = mangaResults
+
+		queryResultJSON, err := json.Marshal(queryResult)
+		if err != nil {
+			return err
+		}
+		fmt.Println(string(queryResultJSON))
 	}
 
 	return nil
