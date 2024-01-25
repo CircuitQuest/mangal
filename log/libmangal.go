c6e6ad52fe11a8cb5a1497bcbc46f210dfddb4d8
David Luevano Alvarado <david@luevano.xyz>
2024-01-24 23:26:05 -0600

    chore: change log format to TimeOnly

--- log/libmangal.go
+++ log/libmangal.go
@@ -11,5 +11,5 @@ var Aggregate = strings.Builder{}
 
 // Log is a convenience function to add log messages to Aggregate in a custom format.
 func Log(s string) {
-	Aggregate.WriteString(fmt.Sprintf("[%s] %s\n", time.Now().Format(time.DateTime), s))
+	Aggregate.WriteString(fmt.Sprintf("[%s] %s\n", time.Now().Format(time.TimeOnly), s))
 }
