CircuitQuest/mangal

commit 542d1b1925a32f0ec52a2a60427178ca60c14c14

feat: add simple notify system for downloaded chapters
to configure, add the discord webhook URL to the config
(run `config write` to get the new keys)

for now only discord is supported (unless somebody wants to
add a different one, it will stay like this)

inline/download.go
@@ -14,28 +14,32 @@ import (
 	"github.com/luevano/mangal/client"
 	"github.com/luevano/mangal/config"
 	"github.com/luevano/mangal/log"
+	"github.com/luevano/mangal/notify"
+	"github.com/luevano/mangal/util/chapter"
 )
 
 func RunDownload(ctx context.Context, args Args) error {
 	client, err := client.NewClientByID(ctx, args.Provider)
 	if err != nil {
-		return err
+		return notify.SendError(err)
 	}
 
 	mangas, err := client.SearchMangas(ctx, args.Query)
 	if err != nil {
-		return err
+		return notify.SendError(err)
 	}
 	if len(mangas) == 0 {
-		return fmt.Errorf("no mangas found with provider ID %q and query %q", args.Provider, args.Query)
+		err = fmt.Errorf("no mangas found with provider ID %q and query %q", args.Provider, args.Query)
+		return notify.SendError(err)
 	}
 
 	mangaResults, err := getSelectedMangaResults(args, mangas)
 	if err != nil {
-		return err
+		return notify.SendError(err)
 	}
 	if len(mangaResults) != 1 {
-		return fmt.Errorf("invalid manga selector %q, needs to select 1 manga only", args.MangaSelector)
+		err = fmt.Errorf("invalid manga selector %q, needs to select 1 manga only", args.MangaSelector)
+		return notify.SendError(err)
 	}
 
 	manga := mangaResults[0].Manga
@@ -57,17 +61,25 @@ func RunDownload(ctx context.Context, args Args) error {
 	if !useMangaMetadata && args.AnilistID != 0 {
 		anilistManga, found, err = client.Anilist().SearchByID(ctx, args.AnilistID)
 		if err != nil {
-			return err
+			return notify.SendError(err)
 		}
 		if !found {
-			return fmt.Errorf("couldn't find anilist manga with id %q (from argument)", args.AnilistID)
+			err = fmt.Errorf("couldn't find anilist manga with id %q (from argument)", args.AnilistID)
+			return notify.SendError(err)
 		}
 		manga.SetMetadata(&anilistManga)
 	}
 
-	chapters, err := getChapters(ctx, client, args, manga)
+	rawChapters, err := getChapters(ctx, client, args, manga)
 	if err != nil {
-		return err
+		return notify.SendError(err)
+	}
+	// wrapper for keeping track of failed/success downloads
+	chapters := make(chapter.Chapters, len(rawChapters))
+	for i, ch := range rawChapters {
+		chapters[i] = &chapter.Chapter{
+			Chapter: ch,
+		}
 	}
 
 	// Take the download options from the config and apply necessary changes
@@ -77,15 +89,14 @@ func RunDownload(ctx context.Context, args Args) error {
 		downloadOptions.SearchMetadata = false
 	}
 
-	totalChapters := len(chapters)
 	// TODO: make configurable
 	maxRetries := 10
 	retryCount := 0
-	for i, chapter := range chapters {
+	for i, ch := range chapters {
 		retry := true
 		for retry {
 			retry = false
-			downChap, err := client.DownloadChapter(ctx, chapter, downloadOptions)
+			downChap, err := client.DownloadChapter(ctx, ch.Chapter, downloadOptions)
 			if err != nil {
 				errMsg := err.Error()
 				// TODO: handle other responses here too if possible
@@ -93,13 +104,15 @@ func RunDownload(ctx context.Context, args Args) error {
 					retry = true
 					retryCount++
 					if retryCount > maxRetries {
-						return fmt.Errorf("exceeded max retries (%d) while downloading chapters", maxRetries)
+						err = fmt.Errorf("exceeded max retries (%d) while downloading chapters", maxRetries)
+						return notify.SendError(err)
 					}
 
 					raTemp := strings.Split(errMsg, ":")
 					raParsed, err := strconv.Atoi(strings.TrimSpace(raTemp[len(raTemp)-1]))
 					if err != nil {
-						return errors.New("error while parsing Retry-Count from error mesage: " + err.Error())
+						err = errors.New("error while parsing Retry-Count from error mesage: " + err.Error())
+						return notify.SendError(err)
 					}
 
 					retryAfter := time.Duration(min(10, raParsed)) * time.Second
@@ -107,13 +120,14 @@ func RunDownload(ctx context.Context, args Args) error {
 					time.Sleep(retryAfter)
 					continue
 				}
-				return err
 			}
+			ch.Down = downChap
+			ch.Err = err
 
 			if args.JSONOutput {
 				dc, err := json.Marshal(downChap)
 				if err != nil {
-					return err
+					return notify.SendError(err)
 				}
 				fmt.Println(string(dc))
 			} else {
@@ -121,10 +135,10 @@ func RunDownload(ctx context.Context, args Args) error {
 			}
 			// To avoid abusing the mangaplus api, since there are no status codes returned to check
 			// sleep for a second on after each chapter download
-			if downChap.ChapterStatus == metadata.DownloadStatusNew && args.Provider == "mango-mangaplus" && i != totalChapters-1 {
+			if downChap.ChapterStatus == metadata.DownloadStatusNew && args.Provider == "mango-mangaplus" && i != len(chapters)-1 {
 				time.Sleep(time.Second)
 			}
 		}
 	}
-	return nil
+	return notify.Send(chapters)
 }

config/config.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/adrg/xdg"
+	"github.com/disgoorg/disgo/webhook"
 	"github.com/luevano/libmangal"
 	"github.com/luevano/mangal/meta"
 	"github.com/luevano/mangal/template/funcs"
@@ -29,15 +30,16 @@ var (
 // Exported config
 var (
 	// Path to the config file
-	Path      = filepath.Join(dir, filename)
-	Icons     = cfg.Icons
-	Cache     = cfg.Cache
-	CLI       = cfg.CLI
-	Read      = cfg.Read
-	Download  = cfg.Download
-	TUI       = cfg.TUI
-	Providers = cfg.Providers
-	Library   = cfg.Library
+	Path         = filepath.Join(dir, filename)
+	Icons        = cfg.Icons
+	Cache        = cfg.Cache
+	CLI          = cfg.CLI
+	Read         = cfg.Read
+	Download     = cfg.Download
+	TUI          = cfg.TUI
+	Providers    = cfg.Providers
+	Library      = cfg.Library
+	Notification = cfg.Notification
 )
 
 func xdgConfig() string {
@@ -481,6 +483,27 @@ func initConfig() config {
 					return expandPath(s)
 				},
 			}),
+		},
+		Notification: configNotification{
+			Discord: configNotificationDiscord{
+				Username: reg(entry[string, string]{
+					Key:         "notification.discord.username",
+					Default:     "Mangal",
+					Description: "Username of the message sender. If empty it will use the configured name in the server.",
+				}),
+				WebhookURL: reg(entry[string, string]{
+					Key:         "notification.discord.webhook_url",
+					Default:     "",
+					Description: "Webhook URL, if present discord notifications will be sent.",
+					Validate: func(s string) error {
+						if s == "" {
+							return nil
+						}
+						_, err := webhook.NewWithURL(s)
+						return err
+					},
+				}),
+			},
 		},
 	}
 	// Load from "default" config paths

config/schema.go
@@ -8,14 +8,15 @@ import (
 )
 
 type config struct {
-	Icons     *entry[string, icon.Type]
-	Cache     configCache
-	CLI       configCLI
-	Read      configRead
-	Download  configDownload
-	TUI       configTUI
-	Providers configProviders
-	Library   configLibrary
+	Icons        *entry[string, icon.Type]
+	Cache        configCache
+	CLI          configCLI
+	Read         configRead
+	Download     configDownload
+	TUI          configTUI
+	Providers    configProviders
+	Library      configLibrary
+	Notification configNotification
 }
 
 type configCLI struct {
@@ -134,4 +135,13 @@ type configProvidersMangaPlus struct {
 
 type configProvidersMangaDex struct {
 	DataSaver *entry[bool, bool]
+}
+
+type configNotification struct {
+	Discord configNotificationDiscord
+}
+
+type configNotificationDiscord struct {
+	Username   *entry[string, string]
+	WebhookURL *entry[string, string]
 }

go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/charmbracelet/bubbletea v0.26.6
 	github.com/charmbracelet/lipgloss v0.12.1
 	github.com/charmbracelet/x/ansi v0.1.4
+	github.com/disgoorg/disgo v0.18.9
 	github.com/fatih/camelcase v1.0.0
 	github.com/getkin/kin-openapi v0.126.0
 	github.com/go-git/go-git/v5 v5.12.0
@@ -57,6 +58,8 @@ require (
 	github.com/charmbracelet/x/term v0.1.1 // indirect
 	github.com/charmbracelet/x/windows v0.1.2 // indirect
 	github.com/cyphar/filepath-securejoin v0.3.0 // indirect
+	github.com/disgoorg/json v1.1.0 // indirect
+	github.com/disgoorg/snowflake/v2 v2.0.3 // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/fatih/color v1.17.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
@@ -81,6 +84,7 @@ require (
 	github.com/sagikazarmark/locafero v0.6.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
+	github.com/sasha-s/go-csync v0.0.0-20240107134140-fcbab37b09ad // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect

go.sum
@@ -88,6 +88,12 @@ github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc/go.mod h1:J7Y8Yc
 github.com/dgryski/trifles v0.0.0-20200323201526-dd97f9abfb48/go.mod h1:if7Fbed8SFyPtHLHbg49SI7NAdJiC5WIA09pe59rfAA=
 github.com/dgryski/trifles v0.0.0-20200830180326-aaf60a07f6a3 h1:JibukGTEjdN4VMX7YHmXQsLr/gPURUbetlH4E6KvHSU=
 github.com/dgryski/trifles v0.0.0-20200830180326-aaf60a07f6a3/go.mod h1:if7Fbed8SFyPtHLHbg49SI7NAdJiC5WIA09pe59rfAA=
+github.com/disgoorg/disgo v0.18.9 h1:Ey1HYrCvdFtxNj8QQE+13wgj7/JTBvf4rXOQcvnBftA=
+github.com/disgoorg/disgo v0.18.9/go.mod h1:TN4tM8VaOaZebO+Ek3686RIATCL32uqPGSNdUYP8yVI=
+github.com/disgoorg/json v1.1.0 h1:7xigHvomlVA9PQw9bMGO02PHGJJPqvX5AnwlYg/Tnys=
+github.com/disgoorg/json v1.1.0/go.mod h1:BHDwdde0rpQFDVsRLKhma6Y7fTbQKub/zdGO5O9NqqA=
+github.com/disgoorg/snowflake/v2 v2.0.3 h1:3B+PpFjr7j4ad7oeJu4RlQ+nYOTadsKapJIzgvSI2Ro=
+github.com/disgoorg/snowflake/v2 v2.0.3/go.mod h1:W6r7NUA7DwfZLwr00km6G4UnZ0zcoLBRufhkFWgAc4c=
 github.com/elazarl/goproxy v0.0.0-20230808193330-2592e75ae04a h1:mATvB/9r/3gvcejNsXKSkQ6lcIaNec2nyfOdlTBR2lU=
 github.com/elazarl/goproxy v0.0.0-20230808193330-2592e75ae04a/go.mod h1:Ro8st/ElPeALwNFlcTpWmkr6IoMFfkjXAvTHpevnDsM=
 github.com/emirpasic/gods v1.18.1 h1:FXtiHYKDGKCW2KzwZKx0iC0PQmdlorYgdFG9jPXJ1Bc=
@@ -330,6 +336,8 @@ github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d h1:hrujxIzL1woJ7
 github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d/go.mod h1:uugorj2VCxiV1x+LzaIdVa9b4S4qGAcH6cbhh4qVxOU=
 github.com/samber/lo v1.46.0 h1:w8G+oaCPgz1PoCJztqymCFaKwXt+5cCXn51uPxExFfQ=
 github.com/samber/lo v1.46.0/go.mod h1:RmDH9Ct32Qy3gduHQuKJ3gW1fMHAnE/fAzQuf6He5cU=
+github.com/sasha-s/go-csync v0.0.0-20240107134140-fcbab37b09ad h1:qIQkSlF5vAUHxEmTbaqt1hkJ/t6skqEGYiMag343ucI=
+github.com/sasha-s/go-csync v0.0.0-20240107134140-fcbab37b09ad/go.mod h1:/pA7k3zsXKdjjAiUhB5CjuKib9KJGCaLvZwtxGC8U0s=
 github.com/sebdah/goldie/v2 v2.5.3 h1:9ES/mNN+HNUbNWpVAlrzuZ7jE+Nrczbj8uFRjM7624Y=
 github.com/sebdah/goldie/v2 v2.5.3/go.mod h1:oZ9fp0+se1eapSRjfYbsV/0Hqhbuu3bJVvKI/NNtssI=
 github.com/segmentio/fasthash v1.0.3 h1:EI9+KE1EwvMLBWwjpRDc+fEM+prwxDYbslddQGtrmhM=

notify/discord/discord.go
@@ -1,0 +1,195 @@
+package discord
+
+import (
+	"strconv"
+	"time"
+
+	"github.com/disgoorg/disgo/discord"
+	"github.com/disgoorg/disgo/webhook"
+	"github.com/luevano/libmangal/metadata"
+	"github.com/luevano/mangal/config"
+	"github.com/luevano/mangal/log"
+	"github.com/luevano/mangal/util/chapter"
+	stringutil "github.com/luevano/mangal/util/string"
+)
+
+const (
+	ColorRed    = 15548997
+	ColorOrange = 15105570
+	ColorGreen  = 5763719
+)
+
+type DiscordNotifyError string
+
+func (e DiscordNotifyError) Error() string {
+	return "error notifying discord: " + string(e)
+}
+
+type DiscordErrorNotifyError struct {
+	toSend error
+	found  error
+}
+
+func (e DiscordErrorNotifyError) Error() string {
+	return "error notifying error (" + e.toSend.Error() + ") to discord: " + e.found.Error()
+}
+
+func chaptersToList(chapters chapter.Chapters, includeDirs bool) (out string) {
+	var lastDir string
+	for i, ch := range chapters {
+		if includeDirs && ch.Down != nil && ch.Down.Directory != lastDir {
+			lastDir = ch.Down.Directory
+			out += lastDir
+			out += ":\n"
+		}
+		out += "> "
+		if ch.Down != nil {
+			switch ch.Down.ChapterStatus {
+			case metadata.DownloadStatusNew:
+				out += "[N]"
+			case metadata.DownloadStatusMissingMetadata:
+				out += "[M]"
+			case metadata.DownloadStatusOverwritten:
+				out += "[O]"
+			case metadata.DownloadStatusSkip:
+				out += "[S]"
+			case metadata.DownloadStatusFailed:
+				out += "[F]"
+			case metadata.DownloadStatusExists:
+				out += "[E]"
+			default:
+				out += "[?]"
+			}
+			out += " "
+		}
+		out += stringutil.FormatFloa32(ch.Chapter.Info().Number)
+		out += " - "
+		out += ch.Chapter.Info().Title
+		if i < len(chapters) {
+			out += "\n"
+		}
+	}
+	return
+}
+
+// Send will send a message using the configured discord WebhookURL.
+func Send(chapters chapter.Chapters) error {
+	client, err := webhook.NewWithURL(config.Notification.Discord.WebhookURL.Get())
+	if err != nil {
+		return DiscordNotifyError(err.Error())
+	}
+	var (
+		color      int
+		summary    string
+		failed     string
+		succeed    string
+		toDownload string
+		fields     []discord.EmbedField
+	)
+
+	d, s, f := chapters.GetEach()
+	switch {
+	case len(f) != 0:
+		color = ColorRed
+	case len(d) != 0:
+		color = ColorOrange
+	default:
+		color = ColorGreen
+	}
+
+	if len(chapters) != 0 {
+		summary += `Downloaded chapters report for manga "` + chapters[0].Chapter.Volume().Manga().Info().Title + `"`
+	}
+
+	// summaries
+	inline := true
+	fields = append(fields, discord.EmbedField{
+		Name:   "succeed",
+		Value:  strconv.Itoa(len(s)),
+		Inline: &inline,
+	})
+	fields = append(fields, discord.EmbedField{
+		Name:   "failed",
+		Value:  strconv.Itoa(len(f)),
+		Inline: &inline,
+	})
+	fields = append(fields, discord.EmbedField{
+		Name:   "to download",
+		Value:  strconv.Itoa(len(d)),
+		Inline: &inline,
+	})
+
+	// failed
+	if len(f) != 0 {
+		failed = chaptersToList(f, false)
+		fields = append(fields, discord.EmbedField{
+			Name:  "failed",
+			Value: failed,
+		})
+	}
+
+	// succeed
+	if len(s) != 0 {
+		succeed = chaptersToList(s, true)
+		fields = append(fields, discord.EmbedField{
+			Name:  "succeed",
+			Value: succeed,
+		})
+	}
+
+	// to download
+	if len(d) != 0 {
+		toDownload = chaptersToList(d, false)
+		fields = append(fields, discord.EmbedField{
+			Name:  "to download",
+			Value: toDownload,
+		})
+	}
+
+	t := time.Now()
+	message, err := client.CreateMessage(discord.WebhookMessageCreate{
+		Username: config.Notification.Discord.Username.Get(),
+		Embeds: []discord.Embed{
+			{
+				Title:       "Downloaded chapters",
+				Description: summary,
+				Color:       color,
+				Timestamp:   &t,
+				Fields:      fields,
+			},
+		},
+	})
+	if err != nil {
+		return DiscordNotifyError(err.Error())
+	}
+	log.Log("sent discord message with id %d", message.ID)
+	return nil
+}
+
+// SendError is a wrapper error handling that will send a webhook
+// message to the configured discord WebhookURL and return the same error back.
+func SendError(toSend error) error {
+	client, err := webhook.NewWithURL(config.Notification.Discord.WebhookURL.Get())
+	if err != nil {
+		return DiscordErrorNotifyError{toSend, err}
+	}
+
+	t := time.Now()
+	message, err := client.CreateMessage(discord.WebhookMessageCreate{
+		Username: config.Notification.Discord.Username.Get(),
+		Embeds: []discord.Embed{
+			{
+				Title: "Error",
+				Description: "General error found when downloading chapters:\n```" +
+					toSend.Error() + "```",
+				Color:     ColorRed,
+				Timestamp: &t,
+			},
+		},
+	})
+	if err != nil {
+		return DiscordErrorNotifyError{toSend, err}
+	}
+	log.Log("sent discord error message with id %d", message.ID)
+	return toSend
+}

notify/notify.go
@@ -1,0 +1,24 @@
+package notify
+
+import (
+	"github.com/luevano/mangal/config"
+	"github.com/luevano/mangal/notify/discord"
+	"github.com/luevano/mangal/util/chapter"
+)
+
+// Send will send a notification to the configured services.
+func Send(chapters chapter.Chapters) error {
+	if config.Notification.Discord.WebhookURL.Get() != "" {
+		return discord.Send(chapters)
+	}
+	return nil
+}
+
+// SendError is a wrapper error handling that will send a notification
+// to configured services and return the same error back.
+func SendError(err error) error {
+	if config.Notification.Discord.WebhookURL.Get() != "" {
+		return discord.SendError(err)
+	}
+	return err
+}

commit 13e00190fe51aa344d85874a6030c1e256610371

fix(inline): panic on any error when downloading
if the error when downloading a chapter is not due to 429 code,
the program panics due to changes introduced in 542d1b1925a32f0ec52a2a60427178ca60c14c14

a complete rewrite of the inline download logic might be needed

inline/download.go
@@ -90,6 +90,7 @@ func RunDownload(ctx context.Context, args Args) error {
 		downloadOptions.SearchMetadata = false
 	}
 
+	// FIX: rework this mess
 	// TODO: make configurable
 	maxRetries := 10
 	retryCount := 0
@@ -97,14 +98,15 @@ func RunDownload(ctx context.Context, args Args) error {
 		retry := true
 		for retry {
 			retry = false
-			downChap, err := client.DownloadChapter(ctx, ch.Chapter, downloadOptions)
-			if err != nil {
-				errMsg := err.Error()
+			ch.Down, ch.Err = client.DownloadChapter(ctx, ch.Chapter, downloadOptions)
+			if ch.Err != nil {
+				errMsg := ch.Err.Error()
 				// TODO: handle other responses here too if possible
 				if strings.Contains(errMsg, "429") && strings.Contains(errMsg, "Retry-After") {
 					retry = true
 					retryCount++
 					if retryCount > maxRetries {
+						// TODO: use as ch.Err?
 						err = fmt.Errorf("exceeded max retries (%d) while downloading chapters", maxRetries)
 						return notify.SendError(err)
 					}
@@ -112,31 +114,38 @@ func RunDownload(ctx context.Context, args Args) error {
 					raTemp := strings.Split(errMsg, ":")
 					raParsed, err := strconv.Atoi(strings.TrimSpace(raTemp[len(raTemp)-1]))
 					if err != nil {
+						// TODO: use as ch.Err?
 						err = errors.New("error while parsing Retry-Count from error mesage: " + err.Error())
 						return notify.SendError(err)
 					}
 
 					retryAfter := time.Duration(min(10, raParsed)) * time.Second
+					// TODO: handle different in case that JSONOutput is desired?
 					fmt.Printf("429 Too Many Requests (retry #%d). Retrying in %s\n", retryCount, retryAfter)
 					time.Sleep(retryAfter)
 					continue
 				}
+
+				// In case that the error is not due to 429 code, just continue to the next chapter
+				// as ch.Down will not be available
+				if args.Provider == "mango-mangaplus" && i != len(chapters)-1 {
+					time.Sleep(time.Second)
+				}
+				continue
 			}
-			ch.Down = downChap
-			ch.Err = err
 
 			if args.JSONOutput {
-				dc, err := json.Marshal(downChap)
+				dc, err := json.Marshal(ch.Down)
 				if err != nil {
 					return notify.SendError(err)
 				}
 				fmt.Println(string(dc))
 			} else {
-				fmt.Println(downChap.Path())
+				fmt.Println(ch.Down.Path())
 			}
 			// To avoid abusing the mangaplus api, since there are no status codes returned to check
-			// sleep for a second on after each chapter download
-			if downChap.ChapterStatus == metadata.DownloadStatusNew && args.Provider == "mango-mangaplus" && i != len(chapters)-1 {
+			// sleep for a second after each chapter download
+			if ch.Down.ChapterStatus == metadata.DownloadStatusNew && args.Provider == "mango-mangaplus" && i != len(chapters)-1 {
 				time.Sleep(time.Second)
 			}
 		}